21838c4cd799d016be9c72a19f1af7db938166c2
BFEIST-CIOSOLUTIONS <48036868+BFEIST-CIOSOLUTIONS@users.noreply.github.com>
2019-06-12 12:17:04 -0700

    Add files via upload

---
+++ AWS_Cloudberry_setup/TekBackup.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace TekBackupSetup
+{
+    partial class frmTekbackup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTekbackup));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 106);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1. Create Bucket";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 158);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2. Create IAM User";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(35, 205);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3. Policy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(35, 250);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 28);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4.  Cloudberry Policy";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(35, 297);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 28);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5. ITGlue";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Client ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ITG ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(68, 34);
+            this.txtID.Margin = new System.Windows.Forms.Padding(2);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 7;
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(68, 62);
+            this.txtCompany.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(100, 20);
+            this.txtCompany.TabIndex = 8;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtOutput.ForeColor = System.Drawing.Color.Lime;
+            this.txtOutput.Location = new System.Drawing.Point(211, 106);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(2);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(385, 221);
+            this.txtOutput.TabIndex = 9;
+            this.txtOutput.Text = "Configure a new client for Tekbackup Services.  Please Complete steps in order.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TekBackupSetup.Properties.Resources.Logo;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(211, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmTekbackup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 347);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtCompany);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmTekbackup";
+            this.Text = "TekBackup Setup";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AWS_Cloudberry_setup/TekBackup.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.IdentityManagement;
+using Amazon.IdentityManagement.Model;
+using Amazon.Auth.AccessControlPolicy;
+using System.Net;
+
+namespace TekBackupSetup
+{
+    partial class frmTekbackup : Form
+    {
+        public string bucketName = "";
+        public string ITGName = "";
+        public string key1 = "";
+        public string key2 = "";
+        public string arn = "";
+        public string policyarn = "";
+        
+        public frmTekbackup()
+        {
+            InitializeComponent(); 
+            
+        }
+
+//Button1  - Create Bucket
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bucketName = txtID.Text;
+            var client = new AmazonS3Client(Amazon.RegionEndpoint.USEast2);
+
+            txtOutput.Text += "\r\n";
+            txtOutput.Text +=  "Creating Bucket: " + bucketName + "\r\n";
+            try
+            {
+                PutBucketRequest putRequest1 = new PutBucketRequest
+                {
+                    BucketName = bucketName
+                };
+                PutBucketResponse response1 = client.PutBucket(putRequest1);
+                txtOutput.Text += response1.ToString() + "\r\n";
+                txtOutput.Text +=  "Bucket Created: arn:aws:s3:::" + bucketName + "\r\n";
+                arn = "arn:aws: s3:::" + bucketName;
+
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                    ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    txtOutput.Text += "Check the provided AWS Credentials." + "\r\n";
+                    txtOutput.Text += "For service sign up go to http://aws.amazon.com/s3" + "\r\n";
+                }
+                else
+                {
+                    txtOutput.Text += "Error Creating Bucket!!!!" + amazonS3Exception.Message + "\r\n";
+                    txtOutput.Text += amazonS3Exception.ErrorCode + "\r\n";
+                }
+            }
+            txtOutput.ScrollToCaret();
+        }
+
+// Button 2 - Create IAM User
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text += "Creating IAM User: " + bucketName + "\r\n";
+            var iamClient2 = new AmazonIdentityManagementServiceClient();
+            try
+            {
+                var readOnlyUser = iamClient2.CreateUser(new CreateUserRequest
+                {
+                    UserName = bucketName,
+                }).User;
+                txtOutput.Text += "IAM USER Created: " + bucketName + "\r\n";  
+            }
+            catch (EntityAlreadyExistsException ex)
+            {
+                txtOutput.Text += ex.Message + "\r\n";
+                var request = new GetUserRequest()
+                {
+                    UserName = bucketName
+                };
+            }
+            try
+            {
+                
+                txtOutput.Text += "Creating Access Key" + "\r\n";
+                var iamClient1 = new AmazonIdentityManagementServiceClient();
+                var accessKey = iamClient1.CreateAccessKey(new CreateAccessKeyRequest
+                {
+             
+                    UserName = bucketName
+                }).AccessKey;
+                txtOutput.Text += "Access Keys Generated:" + "\r\n";
+                txtOutput.Text += accessKey.AccessKeyId + "\r\n";
+                key1 = accessKey.AccessKeyId;
+                txtOutput.Text += accessKey.SecretAccessKey + "\r\n";
+                key2 = accessKey.SecretAccessKey;
+            }
+            catch (LimitExceededException ex)
+            {
+                txtOutput.Text += ex.Message;
+            }
+            txtOutput.ScrollToCaret();
+        }
+
+//Button3 - Policy
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text += "Creating Policy" + "\r\n";
+            var client = new AmazonIdentityManagementServiceClient();
+            string policyDoc = GenerateUserPolicyDocument(bucketName);
+            var request = new CreatePolicyRequest
+            {
+                PolicyName = bucketName + "Policy",
+                PolicyDocument = policyDoc
+            };
+            try
+            {
+                var createPolicyResponse = client.CreatePolicy(request);
+                txtOutput.Text += "Policy named " + createPolicyResponse.Policy.PolicyName + " Created." + "\r\n";
+                policyarn = createPolicyResponse.Policy.Arn;
+            }
+            catch (EntityAlreadyExistsException)
+            {
+                txtOutput.Text += "Policy " + bucketName + " already exits." + "\r\n";
+            }
+
+            txtOutput.Text += "Attaching policy to User" + "\r\n";
+            var attachrequest = new AttachUserPolicyRequest
+            {
+                UserName = bucketName,
+                PolicyArn = policyarn
+            };
+            try
+            {
+                var createPolicyResponse = client.AttachUserPolicy(attachrequest);
+                txtOutput.Text += "Policy applied" + "\r\n";
+            }
+            catch (Exception)
+            {
+                txtOutput.Text += "Attach Failed" + "\r\n";
+            }
+            txtOutput.ScrollToCaret();
+        }
+        public static string GenerateUserPolicyDocument(string bucketName)
+        {
+
+            string resourcearn = "arn:aws:s3:::" + bucketName + "/*";
+            string resourcearn2 = "arn:aws:s3:::" + bucketName;
+            var actionGet = new ActionIdentifier("s3:*");
+            var actions = new List<ActionIdentifier>();
+            actions.Add(actionGet);
+            var resource = new Resource(resourcearn);
+            var resource2 = new Resource(resourcearn2);
+            var resources = new List<Resource>();
+            resources.Add(resource);
+            resources.Add(resource2);
+            var statement = new Amazon.Auth.AccessControlPolicy.Statement(Amazon.Auth.AccessControlPolicy.Statement.StatementEffect.Allow)
+            {
+                Actions = actions,
+                Id = bucketName + "Statmentid",
+                Resources = resources
+
+            };
+            var statements = new List<Amazon.Auth.AccessControlPolicy.Statement>();
+            statements.Add(statement);
+            var policy = new Policy
+            {
+                Id = bucketName + "Policy",
+                Version = "2012-10-17",
+                Statements = statements
+            };
+            return policy.ToJson();
+        }
+
+//Button 5- ITG
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bucketName = txtID.Text;
+            ITGName = txtCompany.Text;         
+            string json = "{ \"data\":{\"type\":\"passwords\",\"attributes\":{\"name\":\"TekBackup_Access_Keys\",\"password\":\""+key2+ "\",\"password-category-id\":32755,\"notes\":\"Password is Secret Key.\r\nAccess Key: " + key1+"\r\nARN: "+arn+ "\r\nPolicy ARN: " + policyarn+" \"}}}";
+            txtOutput.Text += json;       
+            string url = "/organizations/"+ITGName+"/relationships/passwords";
+            string results = "";
+            using (var client = new WebClient())
+            {
+                client.Proxy = null;
+                client.Headers[HttpRequestHeader.Host] = "api.itglue.com";
+                client.Encoding = Encoding.UTF8;                
+                client.Headers[HttpRequestHeader.ContentType] = "application/vnd.api+json";
+                client.Credentials = null;
+                client.Headers.Add("x-api-key","ITG.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                client.BaseAddress = "https://api.itglue.com";
+                results = client.UploadString(url, "POST", json);
+            }
+            txtOutput.Text += results;
+        }
+
+//Button 4 - CloudBerry
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var oldpolicy = "";
+            txtOutput.Text += "Editing CB Role Policy" + "\r\n";
+
+            var client = new AmazonIdentityManagementServiceClient();
+            
+            var getcurrentrequest = new GetRolePolicyRequest
+            {
+                PolicyName = "CloudBerryMBSPolicy",
+                RoleName = "CloudBerryMBSRole-6bba62a8-a0da-497c-b296-2fe588c64db4"
+                
+            };
+            try
+            {
+                var getPolicyResponse = client.GetRolePolicy(getcurrentrequest);
+                oldpolicy = System.Net.WebUtility.UrlDecode(getPolicyResponse.PolicyDocument);
+               
+               
+                
+                
+            }
+            catch (NoSuchEntityException)
+            {
+                txtOutput.Text += "Policy does not exist." + "\r\n";
+            }
+            var newpolicy = oldpolicy.Remove(oldpolicy.Length - 3,3);
+
+            newpolicy += ",{\"Effect\": \"Allow\",  \"Action\": \"s3:*\", \"Resource\": [  \"arn:aws:s3:::"+ txtID.Text + "\"], \"Condition\": {} }, {\"Effect\": \"Allow\",  \"Action\": \"s3:*\", \"Resource\": [  \"arn:aws:s3:::" + txtID.Text + "/*\"], \"Condition\": {} }  ] }";
+         
+
+            var putrolepolicyrequest = new PutRolePolicyRequest
+            {
+                RoleName = "CloudBerryMBSRole-6bba62a8-a0da-497c-b296-2fe588c64db4",
+                PolicyName = "CloudBerryMBSPolicy",
+                PolicyDocument = newpolicy
+            };
+
+            try
+            {
+                var putPolicyResponse = client.PutRolePolicy(putrolepolicyrequest);
+                txtOutput.Text += "CloudBerry Role Policy Update - Success \r\n";
+
+            }
+            catch (NoSuchEntityException)
+            {
+                txtOutput.Text += "Policy does not exist." + "\r\n";
+            }
+        }
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
